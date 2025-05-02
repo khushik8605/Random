04bc3046ec7608154b4ac4c0d034eef5a6dfb633
khushik8605 <khushikashyap8605@gmail.com>
2025-05-02 19:00:45 +0530

    First files added

---
+++ fibonacci.py
@@ -0,0 +1,10 @@
+# fibonacci.py
+
+def fibonacci(n):
+    a, b = 0, 1
+    for _ in range(n):
+        print(a, end=' ')
+        a, b = b, a + b
+
+# Example: Print first 10 Fibonacci numbers
+fibonacci(10)
